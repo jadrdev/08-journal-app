8c5d73b415a30f9cb7740b9b3c065e5a0b39fac4
Joshua <jadrdevcm@gmail.com>
2022-04-25 08:50:25 +0100

    Creación del JournalApp

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import { JournalScreen } from '../components/journal/JournalScreen'
+import { AuthRouter } from './AuthRouter'
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route
+            path="/auth"
+            component={AuthRouter}
+          />
+          <Route
+            exact
+            path="/"
+            component={JournalScreen}
+          />
+
+          
+          <Redirect to="/auth/login" />
+        </Switch>
+      </div>
+    </Router>
+
+  )
+}
